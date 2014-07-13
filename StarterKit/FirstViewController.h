363aaa9c588ffb2f155d84badf661f9a13c0a25b
Harry Ng <harryworld@gmail.com>
2014-07-14 06:13:02 +0800

    Initial Commit

---
+++ StarterKit/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  StarterKit
+//
+//  Created by Harry Ng on 14/7/14.
+//  Copyright (c) 2014 Request. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ StarterKit/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  StarterKit
+//
+//  Created by Harry Ng on 14/7/14.
+//  Copyright (c) 2014 Request. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
